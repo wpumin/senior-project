0a9d128bfb56d74f4492cd3b19ff0b8433c56074
khonjanat <dviver100@gmail.com>
2019-10-02 09:47:47 +0700

    add countstudent database

---
+++ app/Count_Student.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Count_Student extends Model
+{
+    //
+}
---
+++ database/migrations/2019_10_02_024531_create_count__students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('count__students', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('card_id');
+            $table->string('time_card');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('count__students');
+    }
+}
