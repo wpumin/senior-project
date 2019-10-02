9b7eba128ba017dbe2b6f1ecbf5f3df50e023db7
Ashita <Leelitsajja>
2019-10-02 17:13:17 +0700

    Seed Student (V.1)

--- database/seeds/StudentsTableSeeder.php
+++ database/seeds/StudentsTableSeeder.php
@@ -14,13 +14,486 @@ class StudentsTableSeeder extends Seeder
         DB::table('students')->insert([
             'nickname' => 'จ๋าย',
             'price' => 900,
-            'car_id' => 1
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
         ]);
 
         DB::table('students')->insert([
             'nickname' => 'จิน',
             'price' => 900,
-            'car_id' => 2
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'อาย',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'กาย',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ฟ้าใส',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ติว',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'เนย',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'น้ำฟ้า',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'แพรวา',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ทิวทัศ',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'อิ๊ง',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'อัน',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ต้นข้าว',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'น้ำฟ้า',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'กล้า',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ต้นวงบ์',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'โอ๊ต',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'กัณฑ์',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'มะขาม',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ต้นข้าว',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ต้นข้าว ช้าง',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ต้นข้าว ชาติ',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'เกรซ',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'วิว',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'แตงโม',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ผักหวาน',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'กล้า',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'โฟกัส',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'โปรแกรม',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ปันปัน',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'โอปอ',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'เนส',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'เบ',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'จักจั่น',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ไข่ต้ม',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ไข่ข้าว',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ปานเทพ',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'แบม',
+            'price' => 900,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ป็อป',
+            'price' => 700,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ครีม',
+            'price' => 700,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'หมวย',
+            'price' => 700,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ฟลุ๊ค',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'เฟิร์ส',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ปุม',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'นิคม',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 1,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'เกตุ',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'มาย (ซอย 6)',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ก็อป (ซอย 6)',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ไอร์ (ซอย 5)',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ก้อย',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ปุ้ม',
+            'price' => 650,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'พอลล่า',
+            'price' => 600,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'กาย',
+            'price' => 600,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
+        ]);
+
+        DB::table('students')->insert([
+            'nickname' => 'ครีม',
+            'price' => 600,
+            'card_id' => Str::random(8),
+            'car_id' => 2,
+            'status' => 0
+
         ]);
     }
 }
