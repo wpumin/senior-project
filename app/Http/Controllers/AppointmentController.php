1a5c202b7ffab11ac26444e135b2fde986af51f0
khonjanat <dviver100@gmail.com>
2019-10-03 17:12:53 +0700

    make response method.

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
-use App\Student;
 use App\Appointment;
 use LogicException;
 use Validator;
@@ -13,14 +11,15 @@ use Illuminate\Support\Facades\DB;
 class AppointmentController extends Controller
 {
     public function __construct(Request $request)
-        {
-            $this->request = $request;
-        }
+    {
+        $this->request = $request;
+    }
 
     public function createAppointment()
-        {
-            $user = User::all()->toArray();
-            $student = Student::all()->toArray();
+    {
+
+        try {
+
             $validate = Validator::make($this->request->all(), [
                 'user_id' => 'required',
                 'period_time' => 'required',
@@ -30,14 +29,43 @@ class AppointmentController extends Controller
             if ($validate->fails()) {
                 throw new LogicException($validate->errors());
             }
+
             DB::beginTransaction();
+
             $res['member'] = Appointment::create([
                 'user_id' => $this->request->input('user_id'),
                 'period_time' => $this->request->input('period_time'),
                 'date' => $this->request->input('date'),
                 'content' => $this->request->input('content')
-                ]);
+            ]);
+
             DB::commit();
+            return $this->responseRequestSuccess('Success!');
+        } catch (Exception $e) {
+            return response()->json($this->formatResponse($e->getMessage()));
         }
-    
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | response เมื่อข้อมูลส่งถูกต้อง
+    |--------------------------------------------------------------------------
+     */
+    protected function responseRequestSuccess($ret)
+    {
+        return response()->json(['status' => 'success', 'data' => $ret], 200)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    }
+    /*
+    |--------------------------------------------------------------------------
+    | response เมื่อข้อมูลมีการผิดพลาด
+    |--------------------------------------------------------------------------
+     */
+    protected function responseRequestError($status = '', $ret = '', $message = 'Bad request', $statusCode = 200)
+    {
+        return response()->json(['status' => $status, 'data' => $ret, 'error' => $message], $statusCode)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    }
 }
