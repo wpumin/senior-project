4534a622d427a4f32184a9e85bfc18fa629780b8
khonjanat <dviver100@gmail.com>
2019-10-07 15:02:45 +0700

    create migrate file qrcode.

---
+++ database/migrations/2019_10_07_075129_create_qrcode_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQrcodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qrcode', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('price');
+            $table->string('image');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qrcode');
+    }
+}
