b9c0eeee8c9f465fa135bb7904ee7af406cac78b
khonjanat <dviver100@gmail.com>
2019-11-27 14:06:24 +0700

    file update overview.

--- app/Payment_log.php
+++ app/Payment_log.php
@@ -8,11 +8,11 @@ class Payment_log extends Model
 {
     protected $table = 'payment_logs';
     protected $fillable = [
-        'tran_key', 'month', 'year', 'user_id', 'status_id', 'car_id', 'price', 'qr_code', 'bank_id'
+        'tran_key', 'month', 'year', 'user_id', 'status_id', 'car_id', 'price', 'qr_code', 'bank_id', 'created_at'
 
 
     ];
 
     protected $guarded = ['id'];
-    protected $dates = ['created_at', 'updated_at'];
+    // protected $dates = ['created_at', 'updated_at'];
 }
