6bd4050281eafdd053d513997734407cefa7e6b1
ez-top <pumin.wongsiri@gmail.com>
2019-12-03 22:04:08 +0700

    no message

--- resources/views/parent/payment_confirm.blade.php
+++ resources/views/parent/payment_confirm.blade.php
@@ -86,7 +86,7 @@
                 <b>แจ้งชำระเงินสำเร็จ</b>
                 <p>กรุณาตรวจสอบสถานะการชำระเงินของท่านภายใน 24 ชั่วโมง หลังการแจ้งชำระเงิน</p>
                 <div class="modal-button text-center mt-3" >
-                    <a href="{{url('parent/index')}}"><button type="button" class="btn btn-primary">ตกลง</button></a>
+                    <a href="<?php echo "/parent/payment/overview/".$_COOKIE['user_id']; ?>"><button type="button" class="btn btn-primary">ตกลง</button></a>
                     <!-- data-dismiss="modal" -->
                 </div>
             </div>
