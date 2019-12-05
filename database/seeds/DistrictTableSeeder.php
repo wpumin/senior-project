3f15cf7d8c5ec995d000f0b1b6e86fca08d7a5a7
Ashita <Leelitsajja>
2019-12-05 23:06:53 +0700

    update district table seeder

--- database/seeds/DistrictTableSeeder.php
+++ database/seeds/DistrictTableSeeder.php
@@ -13,7 +13,7 @@ class DistrictTableSeeder extends Seeder
     public function run()
     {
         DB::table('districts')->insert([
-            'name' => 'เทศบาลตำบลบ้านไร่',
+            'name' => 'ตำบลบ้านไร่',
             'status' => '1',
             'price' => 900,
             'qr_code' => 'images/qrcode_pay/900.png',
@@ -29,7 +29,7 @@ class DistrictTableSeeder extends Seeder
         ]);
 
         DB::table('districts')->insert([
-            'name' => 'ตำบลหนองหย่าง',
+            'name' => 'ตำบลหนองฝาง',
             'status' => '1',
             'price' => 650,
             'qr_code' => 'images/qrcode_pay/650.png',
@@ -69,35 +69,12 @@ class DistrictTableSeeder extends Seeder
         ]);
 
         DB::table('districts')->insert([
-            'name' => 'ตำบลทุ่งนางาม',
+            'name' => 'ตำบลหนองฉาง',
             'status' => '1',
             'price' => 350,
             'qr_code' => 'images/qrcode_pay/350.png',
             'created_at' => Carbon::now()->format('Y-m-d')
         ]);
 
-        DB::table('districts')->insert([
-            'name' => 'โรงเรียนหนองฉางวิทยา',
-            'status' => '1',
-            'price' => null,
-            'qr_code' => null,
-            'created_at' => Carbon::now()->format('Y-m-d')
-        ]);
-
-        DB::table('districts')->insert([
-            'name' => 'โรงเรียนธรรมานุวัตรวิทยา',
-            'status' => '1',
-            'price' => null,
-            'qr_code' => null,
-            'created_at' => Carbon::now()->format('Y-m-d')
-        ]);
-
-        DB::table('districts')->insert([
-            'name' => 'โรงเรียนวัดหนองขุนชาติ',
-            'status' => '1',
-            'price' => null,
-            'qr_code' => null,
-            'created_at' => Carbon::now()->format('Y-m-d')
-        ]);
     }
 }
