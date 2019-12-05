1e819603cf49966e2935d0859b369b30360a9fc7
khonjanat <dviver100@gmail.com>
2019-12-05 21:00:01 +0700

    update 419 page.

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -12,16 +12,45 @@
                     <p class="text-center my-3"> ขณะนี้บัญชีนี้ถูกใช้ด้วยอุปกรณ์เครื่องอื่นอยู่ <span class="spinner-border"> </span></p>
                     <h1 class="text-center my-3 d-none"> 419 page</h1>
                     <div class="my-3">
-                        <a href="{{ url('/') }}" class="submit-box d-block w-100">กลับสู่หน้าหลัก</a>
+                        <a href="{{ url('/') }}" onclick="deleteAllCookies()" class="submit-box d-block w-100">กลับสู่หน้าหลัก</a>
                     </div>
                     <div class="mt-4 text-center">
-                        <a class="small-ps underline" href="javascript:history.back()">ย้อนกลับ</a>
+                        {{-- <a class="small-ps underline" href="javascript:history.back()">ย้อนกลับ</a> --}}
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+
+<script>
+
+function deleteAllCookies() {
+
+var res = document.cookie;
+var multiple = res.split(";");
+
+
+for (var i = 0; i < multiple.length; i++) {
+
+    var key = multiple[i].split("=");
+    document.cookie = key[0] + " =; expires = Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+}
+
+for (var i = 0; i < multiple.length; i++) {
+
+    var key = multiple[i].split("=");
+    document.cookie = key[0] + " =; expires = Thu, 01 Jan 1970 00:00:00 UTC; ;";
+}
+
+}
+
+
+</script>
+
 @endsection
 
 
